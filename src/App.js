3e4950d1262242f24c70cc9e29e87274a93e1f2d
Kimberly Soulef <dev.kimberlyferreira@gmail.com>
2023-05-01 00:36:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import { FiSearch } from 'react-icons/fi'
+import './style.css';
+import backgroundimg from './img/backgroundimg.jpg';
+import api from './SERVICES/api';
 
 function App() {
+
+  function App() {
+    return (
+      <div className='backgroundimg'
+        style={{
+          backgroundimg: `url(${backgroundimg})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          height: '70vh',
+        }}
+      >
+        <h1>Minha aplicação com imagem de background</h1>
+      </div>
+    );
+  }
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState('')
+
+ async function handleSearch(){
+
+    if(input === ''){
+      alert('Preencha o seu CEP')
+      return;
+      }
+
+      try{
+        const response = await api.get(`${input}/json`);
+        setCep(response.data)
+        setInput('')
+
+      }catch{
+        alert('Ops! Erro ao buscar, certifique-se que o CEP foi digitado corretamente e tente novamente!')
+        setInput("")
+      }
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <h1 className="title">Buscador CEP</h1>
+ 
+    <div className="containerInput">
+    <input
+    type="text"
+    placeholder="Digite seu CEP..."
+    value={input}
+    onChange={(e) => setInput(e.target.value)}
+    />
+
+
+    <button className="buttonSearch" onClick={handleSearch}>
+      <FiSearch size={25} color="FFF"/>
+    </button>
+    </div>
+
+    {Object.keys(cep).length > 0 && (
+    <main className='main'>
+          <h2>CEP: {cep.cep}</h2>
+          <span>{cep.logradouro}</span>
+          <span>Bairro: {cep.bairro}</span>
+          <span>Cidade: {cep.localidade}</span>
+          <span>Estado: {cep.uf}</span>
+          <span>DDD: {cep.ddd}</span>
+    
+    </main>
+    )}
+
     </div>
   );
 }
